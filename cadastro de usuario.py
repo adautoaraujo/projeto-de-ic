0422ff832d6d528953627384fdd88f79f2985bf6
adautoaraujo <mradauto@gmail.com>
2017-09-15 20:33:41 -0300

    Update cadastro de usuario.py

--- cadastro de usuario.py
+++ cadastro de usuario.py
@@ -3,7 +3,7 @@ senha=input()
 if(cpf=="mateus")and(senha=="mateus"):
     print("Seja bem-vindo Mateus")
     novo_usuario=input("Insira o primeiro nome do novo usuario:")
-    cpf_usuario=input("Insira o cpf do usuario:")
+    cpf_usuario=input("Insira os numeros do cpf do usuario:")
     while(len(cpf_usuario)!=11):
         print("Cpf invalido")
         cpf_usuario=int(input("Insira o cpf do usuario sem espaços ou separadores:"))
@@ -11,4 +11,3 @@ if(cpf=="mateus")and(senha=="mateus"):
     while(len(senha_usuario)!=6):
         print("Senha invalida")
         senha_usuario=int(input("Insira senha numerica de 6 digitos:"))
-
